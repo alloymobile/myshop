5be737b29d9a5d24fa8a1aeefaf26134f795df1a
Tapas Mandal <alloymobileinc@gmail.com>
2023-03-18 12:56:33 -0400

    Added dynamic code

--- myshop/myshop/Form1.cs
+++ myshop/myshop/Form1.cs
@@ -13,9 +13,22 @@ namespace myshop
     public partial class Form1 : Form
     {
         Product product = new Product(1, "IPhone 14 Pro", 1200, 10);
+        List<string> name = new List<string>();
         public Form1()
         {
             InitializeComponent();
+            name.Add("Hello");
+            name.Add("Good");
+            int x1 = 200;
+            name.ForEach(x =>
+            {
+                Button b1 = new Button();
+                b1.Text = x;
+                b1.Location = new Point(x1,120);
+                this.Controls.Add(b1);
+                x1 = x1 + 100;
+            });
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
